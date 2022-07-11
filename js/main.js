cff90489293e8457f67bcf760dcabffaa2f3df63
T246-Edu <tawfik.2419010@stemdakahlia.moe.edu.eg>
2022-07-11 16:03:34 +0200

    first_commit

---
+++ js/main.js
@@ -0,0 +1,281 @@
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+let jsonData = {};
+async function setDataUsers() {
+  await localStorage.setItem("players-scores", JSON.stringify(jsonData));
+}
+function setUserScore() {
+  let user_one_score = document.getElementById("score-id-one");
+  let user_two_score = document.getElementById("score-id-two");
+  user_one_score.innerText = jsonData["Player 1"];
+  user_two_score.innerText = jsonData["Player 2"];
+}
+async function getUsersScores() {
+  jsonData = await JSON.parse(localStorage.getItem("players-scores"));
+  if (!jsonData) {
+    jsonData = { "Player 1": 0, "Player 2": 0 };
+    await setDataUsers();
+  }
+  setUserScore();
+}
+
+const light = new THREE.AmbientLight(0xffffff);
+scene.add(light);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.3);
+directionalLight.castShadow = true;
+scene.add(directionalLight);
+directionalLight.position.set(0, 1, 1);
+
+camera.position.z = 5;
+renderer.setClearColor(0xb7c3f3, 1);
+
+const loader = new THREE.GLTFLoader();
+let doll;
+
+const start_position = 6;
+const end_position = -start_position;
+
+const text = document.querySelector(".text");
+
+let DEAD_PLAYERS = 0;
+let SAFE_PLAYERS = 0;
+
+const startBtn = document.querySelector(".start-btn");
+
+//musics
+const bgMusic = new Audio("../assets/music/bg.mp3");
+bgMusic.loop = true;
+const winMusic = new Audio("../assets/music/win.mp3");
+const loseMusic = new Audio("../assets/music/lose.mp3");
+
+loader.load("../models/squid_game_doll/scene.gltf", function (gltf) {
+  scene.add(gltf.scene);
+  doll = gltf.scene;
+  gltf.scene.position.set(0, -1, 0);
+  gltf.scene.scale.set(0.4, 0.4, 0.4);
+  startBtn.innerText = "start";
+});
+
+function lookBackward() {
+  gsap.to(doll.rotation, { duration: 0.45, y: -3.15 });
+  setTimeout(() => (dallFacingBack = true), 150);
+}
+function lookForward() {
+  gsap.to(doll.rotation, { duration: 0.45, y: 0 });
+  setTimeout(() => (dallFacingBack = false), 450);
+}
+
+async function endTheGame() {
+  await delay(2500);
+  text.innerText = "Restarting game...";
+  await delay(1500);
+  location.reload();
+}
+
+function createCube(size, posX, rotY = 0, color = 0xfbc851) {
+  const geometry = new THREE.BoxGeometry(size.w, size.h, size.d);
+  const material = new THREE.MeshBasicMaterial({ color });
+  const cube = new THREE.Mesh(geometry, material);
+  cube.position.set(posX, 0, 0);
+  cube.rotation.y = rotY;
+  scene.add(cube);
+  return cube;
+}
+
+//Creating runway
+createCube(
+  { w: start_position * 2 + 0.21, h: 1.5, d: 1 },
+  0,
+  0,
+  0xe5a716
+).position.z = -1;
+createCube({ w: 0.2, h: 1.5, d: 1 }, start_position, -0.4);
+createCube({ w: 0.2, h: 1.5, d: 1 }, end_position, 0.4);
+
+class Player {
+  constructor(name = "Player", radius = 0.25, posY = 0, color = 0xffffff) {
+    const geometry = new THREE.SphereGeometry(radius, 100, 100);
+    const material = new THREE.MeshBasicMaterial({ color });
+    const player = new THREE.Mesh(geometry, material);
+    scene.add(player);
+    player.position.x = start_position - 0.4;
+    player.position.z = 1;
+    player.position.y = posY;
+    this.player = player;
+    this.playerInfo = {
+      positionX: start_position - 0.4,
+      velocity: 0,
+      name,
+      isDead: false,
+    };
+  }
+
+  run() {
+    if (this.playerInfo.isDead) return;
+    this.playerInfo.velocity = 0.03;
+  }
+
+  stop() {
+    gsap.to(this.playerInfo, { duration: 0.1, velocity: 0 });
+  }
+
+  async check() {
+    await getUsersScores();
+    if (this.playerInfo.isDead) return;
+    if (!dallFacingBack && this.playerInfo.velocity > 0) {
+      text.innerText = this.playerInfo.name + " lost!!!";
+      this.playerInfo.isDead = true;
+      this.stop();
+      DEAD_PLAYERS++;
+      loseMusic.play();
+      if (DEAD_PLAYERS == players.length) {
+        text.innerText = "Everyone lost!!!";
+        gameStat = "ended";
+        await endTheGame();
+      }
+      if (DEAD_PLAYERS + SAFE_PLAYERS == players.length) {
+        gameStat = "ended";
+        await endTheGame();
+      }
+    }
+    if (this.playerInfo.positionX < end_position + 0.7) {
+      text.innerText = this.playerInfo.name + " is safe!!!";
+      jsonData[this.playerInfo.name]++;
+      await setDataUsers();
+
+      this.playerInfo.isDead = true;
+      this.stop();
+      SAFE_PLAYERS++;
+      winMusic.play();
+      if (SAFE_PLAYERS == players.length) {
+        text.innerText = "Everyone is safe!!!";
+        gameStat = "ended";
+        await endTheGame();
+      }
+      if (DEAD_PLAYERS + SAFE_PLAYERS == players.length) {
+        gameStat = "ended";
+        await endTheGame();
+      }
+    }
+  }
+
+  update() {
+    this.check();
+    this.playerInfo.positionX -= this.playerInfo.velocity;
+    this.player.position.x = this.playerInfo.positionX;
+  }
+}
+
+async function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const player1 = new Player("Player 1", 0.25, 0.3, 0xd1ffc6);
+const player2 = new Player("Player 2", 0.25, -0.3, 0xffcfd2);
+
+const players = [
+  {
+    player: player1,
+    key: "ArrowUp",
+    name: "Player 1",
+  },
+  {
+    player: player2,
+    key: "w",
+    name: "Player 2",
+  },
+];
+
+const TIME_LIMIT = 15;
+async function init() {
+  text.innerText = "Starting in 3";
+  await delay(1000);
+  text.innerText = "Starting in 2";
+  await delay(1000);
+  text.innerText = "Starting in 1";
+  await delay(1000);
+  lookBackward();
+  text.innerText = "Gooo!!!";
+  bgMusic.play();
+  start();
+}
+
+let gameStat = "loading";
+
+function start() {
+  gameStat = "started";
+  const progressBar = createCube({ w: 8, h: 0.33, d: 1 }, 0, 0, 0xebaa12);
+  progressBar.position.y = 3.15;
+  gsap.to(progressBar.scale, { duration: TIME_LIMIT, x: 0, ease: "none" });
+  setTimeout(async () => {
+    if (gameStat != "ended") {
+      text.innerText = "Time Out!!!";
+      loseMusic.play();
+      gameStat = "ended";
+      await endTheGame();
+    }
+  }, TIME_LIMIT * 1000);
+  startDall();
+}
+
+let dallFacingBack = true;
+async function startDall() {
+  lookBackward();
+  await delay(Math.random() * 1500 + 1500);
+  lookForward();
+  await delay(Math.random() * 750 + 750);
+  startDall();
+}
+
+startBtn.addEventListener("click", () => {
+  if (startBtn.innerText == "START") {
+    init();
+    document.querySelector(".modal").style.display = "none";
+  }
+});
+
+function animate() {
+  renderer.render(scene, camera);
+  players.map((player) => player.player.update());
+  if (gameStat == "ended") return;
+  requestAnimationFrame(animate);
+}
+animate();
+
+window.addEventListener("keydown", function (e) {
+  if (gameStat != "started") return;
+  let p = players.find((player) => player.key == e.key);
+  if (p) {
+    p.player.run();
+  }
+});
+window.addEventListener("keyup", function (e) {
+  let p = players.find((player) => player.key == e.key);
+  if (p) {
+    p.player.stop();
+  }
+});
+
+window.addEventListener("resize", onWindowResize, false);
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+document.querySelector(".refresh-ico").addEventListener("click", async () => {
+  jsonData = { "Player 1": 0, "Player 2": 0 };
+  await setDataUsers();
+  setUserScore();
+});
